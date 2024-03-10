873d49f5a3deb68593cd0c00e44415a132a8e53d
Eric-NoBraincells <eeeric4321@gmail.com>
2024-03-09 21:37:29 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <main className="">
       <section className="w-full flex justify-center bg-slate-900 flex-col items-center pt-6">
-        <Image src="/team_pics/main-banner3.jpg" width={5000} height={10} className="w-4/5 lg:w-2/4 mb-10"/>
-        <h1 className="text-lg sm:text-2xl md: xl:text-7xl font-semibold mt-8 mb-2">
+        <Image
+          src="/team_pics/main-banner3.jpg"
+          width={5000}
+          height={10}
+          className="w-4/5 lg:w-2/4"
+        />
+        <h1 className="text-4xl font-semibold mt-8 mb-2">
           Planetary Drive Robotics
         </h1>
         <p className="mb-4">First Robotics Team #2856</p>
