ec3cbbcaa302b0beaee8dd5c61d26ef0826c2538
Eric-NoBraincells <eeeric4321@gmail.com>
2024-03-09 19:13:16 -0500

    Update - Eric
    Updated navbar theme, changed icons to svg.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -63,15 +63,36 @@ export default function Navbar() {
   return (
     <div className={`${mNav ? "" : "sticky top-0"} z-50`}>
       <div
-        className={`${openNav()} z-10 absolute h-full bg-neutral-700 left-0 flex flex-col items-center w-full`}
+        className={`${openNav()} z-10 absolute h-full bg-slate-950 left-0 flex flex-col items-center w-full`}
       >
-        <Image
+        <svg
+          onClick={() => setMNav((prev) => !prev)}
+          className="cursor-pointer absolute top-3 right-2 fill-gray-300 stroke-gray-300"
+          width="35px"
+          height="35px"
+          viewBox="-0.5 0 25 25"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3 21.32L21 3.32001"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M3 3.32001L21 21.32"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        {/* <Image
           onClick={() => setMNav((prev) => !prev)}
           src="/menu-close.png"
           height={35}
           width={35}
           className="cursor-pointer absolute top-3 right-2"
-        />
+        /> */}
         <h2 className="m-4 text-2xl font-semibold">Planetary Drive</h2>
         {pages.map((link) => (
           <Link
@@ -153,13 +174,28 @@ export default function Navbar() {
        * BELOW IS CODE FOR MAIN NAV BAR*/}
 
       <div className="w-full bg-slate-950 h-16 flex justify-center items-center relative mb-0 p-0">
-        <Image
+        <svg
+          onClick={() => setMNav((prev) => !prev)}
+          className="left-5 absolute md:hidden cursor-pointer fill-gray-300 stroke-gray-300"
+          width="40px"
+          height="40px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M5 7C5 6.44772 5.44772 6 6 6H18C18.5523 6 19 6.44772 19 7C19 7.55228 18.5523 8 18 8H6C5.44772 8 5 7.55228 5 7ZM5 12C5 11.4477 5.44772 11 6 11H18C18.5523 11 19 11.4477 19 12C19 12.5523 18.5523 13 18 13H6C5.44772 13 5 12.5523 5 12ZM5 17C5 16.4477 5.44772 16 6 16H18C18.5523 16 19 16.4477 19 17C19 17.5523 18.5523 18 18 18H6C5.44772 18 5 17.5523 5 17Z"
+          />
+        </svg>
+        {/* <Image
           onClick={() => setMNav((prev) => !prev)}
           src="/menu-icon.png"
           height={35}
           width={35}
           className="left-5 absolute md:hidden cursor-pointer"
-        />
+        /> */}
         <Link href="/" className="absolute md:left-[8%] lg:left-[10%]">
           <div className="flex md:items-center ">
             <Image src="/logo.png" height={50} width={50}></Image>
@@ -187,16 +223,26 @@ export default function Navbar() {
           >
             More{" "}
             <span>
-              <Image
-                src="/dropdown.png"
-                className="absolute top-[15%] transition duration-500 group-hover:-rotate-180"
-                height={20}
-                width={20}
-              />
+              <svg
+                className={`absolute top-[15%] transition duration-500 group-hover:-rotate-180 fill-neutral-300 stroke-neutral-300 hover:fill-neutral-100 hover:stroke-neutral-300 ${
+                  otherPages.includes(pathname) && "text-white"
+                }`}
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
+                />
+              </svg>
             </span>
           </p>
           <div className="pointer-events-none	group-hover:pointer-events-auto	overflow-hidden absolute left-5 top-full">
-            <div className="opacity-0 bg-neutral-700 w-44 p-4 -translate-y-full group-hover:translate-y-0 transition-transform duration-500 group-hover:opacity-100">
+            <div className="opacity-0 bg-slate-950 w-44 p-4 -translate-y-full group-hover:translate-y-0 transition-transform duration-500 group-hover:opacity-100">
               <ul>
                 <li className="py-2 text-neutral-300 transition duration-100 hover:text-neutral-100">
                   <Link href="/join">Join</Link>
