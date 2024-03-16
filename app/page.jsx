29494a8775baf2e76cfc9256c7e55da7bf1f28ef
witherhoard99 <151796337+witherhoard99@users.noreply.github.com>
2024-03-16 18:56:38 -0400

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -11,22 +11,28 @@ export default function Home() {
           width={5000}
           height={10}
           className="w-auto lg:w-full"
-        ></Image>
+          alt={"group picture of robotics team in front of a river in knoxville"}>
+        </Image>
+
         <h1 className="text-center text-8xl font-semibold mt-6 mb-2">
           Planetary Drive Robotics
         </h1>
+
         <p className="text-center	text-2xl mb-4 m-2">
           First Robotics Team #2856
         </p>
+
         <h2 className="text-xl mt-2">
           Thanks to all the sponsors that allow us to keep working!
         </h2>
+
         <Link href="/sponsors">
           <Button variant="outline" className="dark m-4">
             Our Sponsors
           </Button>
         </Link>
       </section>
+
       <section className="flex flex-col lg:flex-row justify-center items-center mt-8 ">
         <div className="lg:mr-20 flex flex-col justify-center">
           <h2 className="text-center text-4xl font-semibold mb-2">Our Team</h2>
@@ -49,25 +55,31 @@ export default function Home() {
           width={100000}
           height={100}
           className="w-1/2 mt-8 lg:mt-0 lg:w-1/3 lg:ml-20 rounded-md"
-        ></Image>
+          alt={"two team members working on the robot at the competetion"}>
+        </Image>
       </section>
+
       <section className="flex flex-col lg:flex-row justify-center items-center mt-16">
         <Image
-          src="/home/home2.png"
+          src="/robot2024NotCropped.jpg"
           width={100000}
           height={100}
-          className="w-1/2 mt-8 lg:mt-0 lg:w-1/3 lg:ml-20 rounded-md"
-        ></Image>
+          className="w-1/2 mt-8 lg:mt-0 lg:w-1/3 rounded-md"
+          alt={"a team member next to the robot at the competetion"}>
+        </Image>
+
         <div className="lg:ml-20 flex flex-col justify-center">
           <h2 className="text-center text-4xl font-semibold mb-2">
             Our Sponsors
           </h2>
+
           <p className="text-center  w-96  leading-loose">
-            The sponsors of Team Planteray Drive are the ones who allow our team
+            The sponsors of Team Planetary Drive are the ones who allow our team
             to build a robot and compete each year in FRC! Sponsoring us comes
             with many benefits. We encourage you to check out our sponsors. If
             you are willing to sponsor us, please contact us!
           </p>
+
           <p className="max-w-96 flex flex-col items-center mt-4">
             Special thanks to our current biggest sponsor:{" "}
             <span className="">
@@ -75,20 +87,22 @@ export default function Home() {
                 <Button
                   variant="link"
                   className="dark p-0 m-0 text-md font-semibold"
-                  size="lg"
-                >
+                  size="lg">
                   Schneider Electric
                 </Button>
               </Link>
             </span>
           </p>
+
           <Link className="dark self-center m-4" href="/sponsors">
             <Button variant="outline" size="lg">
               Our Sponsors
             </Button>
           </Link>
+          
         </div>
       </section>
+
     </main>
   );
 }
